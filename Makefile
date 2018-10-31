f5b67adddd0a957347c90651cbfdc5290b3ae6dd
Toshiyuki Hirooka <toshi.hirooka@gmail.com>
2018-11-01 01:12:16 +0900

    first commit

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	clang++ -x objective-c++ -framework Foundation -framework AppKit -framework Metal -framework MetalKit -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/ -I/usr/local/include -L/usr/local/lib -I/opt/X11/include -lSDL2 main.cpp
---
+++ main.cpp
@@ -0,0 +1,85 @@
+#include <Metal/Metal.h>
+#include <QuartzCore/CAMetalLayer.h>
+#include <SDL2/SDL.h>
+
+int main(int argc, char **argv) {
+  SDL_Init(SDL_INIT_EVERYTHING);
+  auto window = SDL_CreateWindow(
+      "title", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480,
+      SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
+
+  auto renderer = SDL_CreateRenderer(window, 1, SDL_RENDERER_ACCELERATED);
+
+  bool firsttime = true;
+
+  while (true) {
+    SDL_Event event;
+    SDL_PollEvent(&event);
+    if (event.type == SDL_QUIT)
+      break;
+
+    auto layer = (CAMetalLayer *)SDL_RenderGetMetalLayer(renderer);
+    id<MTLRenderCommandEncoder> encoder =
+        (id<MTLRenderCommandEncoder>)SDL_RenderGetMetalCommandEncoder(renderer);
+
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+
+    SDL_RenderClear(renderer);
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+
+    SDL_Rect r{0, 0, 0, 0};
+    SDL_RenderFillRect(renderer, &r);
+
+    {
+      SDL_Rect rect{50, 50, 50, 50};
+      const float verts[] = {(float)rect.x,
+                             (float)rect.y + (float)rect.h,
+                             (float)rect.x,
+                             (float)rect.y,
+                             (float)rect.x + (float)rect.w,
+                             (float)rect.y + (float)rect.h,
+                             (float)rect.x + (float)rect.w,
+                             (float)rect.y};
+
+      float color[4] = {0.f, 1.f, 1.f, 1.f};
+      [encoder setFragmentBytes:color length:sizeof(color) atIndex:0];
+      [encoder setVertexBytes:verts length:sizeof(verts) atIndex:0];
+      [encoder drawPrimitives:MTLPrimitiveTypeTriangleStrip
+                  vertexStart:0
+                  vertexCount:4];
+    }
+
+    {
+      SDL_Rect rect{100, 100, 50, 50};
+      const float verts[] = {(float)rect.x,
+                             (float)rect.y + (float)rect.h,
+                             (float)rect.x,
+                             (float)rect.y,
+                             (float)rect.x + (float)rect.w,
+                             (float)rect.y + (float)rect.h,
+                             (float)rect.x + (float)rect.w,
+                             (float)rect.y};
+
+      float color[4] = {1.f, 1.f, 0.f, 1.f};
+      [encoder setFragmentBytes:color length:sizeof(color) atIndex:0];
+      [encoder setVertexBytes:verts length:sizeof(verts) atIndex:0];
+      [encoder drawPrimitives:MTLPrimitiveTypeTriangleStrip
+                  vertexStart:0
+                  vertexCount:4];
+    }
+
+    SDL_RenderPresent(renderer);
+    SDL_Delay(50);
+
+    if (firsttime) {
+      SDL_SetWindowSize(window, 640, 480);
+      firsttime = false;
+    }
+  }
+
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+
+  return EXIT_SUCCESS;
+}
